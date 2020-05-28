3100c1723f95ca6e3da5fe18a88a0b875a5c73e5
Кирилл Войтулевич <voytulevich.k@gmail.com>
2020-05-28 10:16:10 +0300

    micromodal lib, add method

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   resources :messages, except: :new
   get 'messages/:id/reply', to: 'messages#new', as: :new_message
+  post 'messages/:id/lost_prescription', to: 'messages#lost_prescription', as: :lost_prescription
 
 end
